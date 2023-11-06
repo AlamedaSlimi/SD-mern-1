415c1e118ac58eda55ebfb8bcfbd715461ea86d8
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-05 17:41:06 -0800

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const uri = 'mongodb+srv://samuel:ChB4t2o8U4EjvgJY@notesapp.g2qgarw.mongodb.net/notesapp?retryWrites=true&w=majority';
-mongoose.connect(uri).then(db => console.log('db is conected')).catch(err => console.log(err));
+mongoose.connect(uri).then(db => console.log('db is connected mf')).catch(err => console.log(err));
 
 
 module.exports = mongoose;
